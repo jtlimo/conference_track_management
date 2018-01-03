74306a1a71f2b7963cd532b603ebd983bd12fcd8
Jessica <jlima@thoughtworks.com>
2018-01-03 16:39:20 -0200

    feat: create new supports methods to create a talk

--- Talks.py
+++ Talks.py
@@ -1,3 +1,4 @@
+import re
 from datetime import datetime, timedelta
 
 #classe que cria e valida as talks;
@@ -5,20 +6,26 @@ from datetime import datetime, timedelta
 A duração das palestras devem ser em minutos;
 A duração da menor palestra será as lightnings que são de 5 minutos.
 O título das palestras não contém números.
-Os apresentadores são pontuais, sem espaço entre as palestras.
 """
 
 class Talks:
 
-    def __init__(self, duration, title):
-        self.duration = duration
+    def __init__(self, title, duration):
         self.title = title
+        self.duration = duration
 
-    def create_talk(duration, title):
-        get_a_time_to_talk(duration)
-
+    def create_talk(title, duration):
+        ##Pq tenho que colocar Talks.funcao?!?
+        if Talks.check_title_contains_numbers(title) > 0:
+            raise ValueError
+        else:
+            return {'title': title, 'time_of_talk': Talks.get_a_time_to_talk(duration)}
 
     def get_a_time_to_talk(duration):
         actual_time = datetime.now()
         time_of_talk = actual_time - timedelta(minutes=duration)
         return time_of_talk.strftime("%I:%M%p") , time_of_talk
+
+    def check_title_contains_numbers(title):
+        regex_pattern = r'[\d]'
+        return len(re.findall(regex_pattern, title))
