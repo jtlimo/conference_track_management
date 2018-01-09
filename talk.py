f6a01ee8846030a557761a9841be76f3b7a31121
Jessica Lima <jtl.limao@gmail.com>
2018-01-09 15:25:44 -0200

    refactor: remove comments

--- talk.py
+++ talk.py
@@ -1,12 +1,6 @@
-import re
-from datetime import datetime, timedelta
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
-#classe que cria e valida as talks;
-"""
-A duração das palestras devem ser em minutos;
-A duração da menor palestra será as lightnings que são de 5 minutos.
-O título das palestras não contém números.
-"""
 
 class Talk:
 
