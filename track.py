69cf5b05387e78183137f8eb27977f399f87e64e
Jessica <jlima@thoughtworks.com>
2018-01-23 18:54:07 -0200

    feature: removing creation of network event from init

--- track.py
+++ track.py
@@ -34,7 +34,7 @@ class Track:
         self.end_lunch = date.replace(hour=13, minute=0, second=0,
                                       microsecond=0)
         self.__schedule_lunch_hour()
-        self.__schedule_network_event()
+        #self.__schedule_network_event()
 
     def is_valid(self):
         self.filtered_hours = []
@@ -53,8 +53,8 @@ class Track:
         scheduled = ScheduledTalk(Talk('Lunch', 60), self.lunch_hour)
         self.scheduled_talks.append(scheduled)
 
-    def __schedule_network_event(self):
-        scheduled = ScheduledTalk(Talk('Network', 60), self.deadline_talk)
+    def schedule_network_event(self, last_talk_of_track):
+        scheduled = ScheduledTalk(Talk('Network', 60), last_talk_of_track.duration)
         self.scheduled_talks.append(scheduled)
 
     def get_scheduled_talks(self):
