70544cb72b843d58f30450641c82a88c4a83da2f
Jessica <jlima@thoughtworks.com>
2018-01-30 20:01:29 -0200

    refactor: removed str method

--- talk.py
+++ talk.py
@@ -36,6 +36,3 @@ class Talk:
     def __init__(self, title, duration):
         self.title = title
         self.duration = duration
-
-    def __str__(self):
-        return self.title + " " + str(self.duration)
