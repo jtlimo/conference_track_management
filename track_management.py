fb5d9ba1aac1ee703705fb02f40600d1c14e18db
Jessica <jlima@thoughtworks.com>
2018-01-30 20:03:55 -0200

    refactor: remove complexity of generate track to talks

--- track_management.py
+++ track_management.py
@@ -10,29 +10,38 @@ class TrackManagement:
     def __init__(self, talks):
         self.talks = talks
 
-    # TODO: Generate 1...n tracks
     def generate_tracks_to_talks(self):
-        track = Track(datetime.now())
-        tracks = [track]
-        for talk in self.talks:
+        track1 = Track(datetime.now())
+        track2 = Track(datetime.now())
+        talks = self.talks
+
+        while talks:
+            talk = talks[0]
+            talks = talks[1:]
             try:
-                track.schedule_talk(talk)
+                if not track1.is_valid():
+                    track1.schedule_talk(talk)
+                else:
+                    print('safada', talk)
+                    track2.schedule_talk(talk)
             except NoEnoughtSpace:
-                print('----------------------------------')
-                new_track = Track(datetime.now())
-                tracks.append(new_track)
-                # FIXME: Move changes to list of talks to Talk class
-                new_track.schedule_talk(talk)
+                talks = self.get_next_talks_iteration([track1,track2], talks, talk)
 
-        self.schedule_network_for_all_tracks(tracks)
+        self.schedule_network_and_lunch_for_all_tracks([track1, track2])
+        return track1, track2
 
-        for index, track in enumerate(tracks):
-            print('Track:', index)
-            for talk in track.get_scheduled_talks():
-                print(talk.get_title(), talk.get_hour(),
-                      talk.get_formatted_end_hour())
-        return tracks
 
-    def schedule_network_for_all_tracks(self, tracks):
+    def schedule_network_and_lunch_for_all_tracks(self, tracks):
         for track in tracks:
             track.schedule_network_event()
+            track.schedule_lunch_hour()
+
+    def get_next_talks_iteration(self, tracks, talks, current_talk):
+        track1, track2 = tracks
+        added_talks = track1.get_scheduled_talks() + \
+                      track2.get_scheduled_talks() + [current_talk]
+        first_added = added_talks[0]
+        size_talks = len(added_talks)
+        head_talks = added_talks[:size_talks - 1]
+        talks.append(first_added)
+        return head_talks + talks
