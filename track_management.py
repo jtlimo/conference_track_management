abf9b371ecb2adff75d4c20b54d2bd6c58da1664
Jessica <jlima@thoughtworks.com>
2018-01-31 15:55:10 -0200

    refactor: removed unused methods

--- track_management.py
+++ track_management.py
@@ -3,6 +3,7 @@
 from track import Track, NoEnoughtSpace
 from datetime import datetime, timedelta
 from uuid import uuid4
+import random
 
 
 class TrackManagement:
@@ -16,7 +17,6 @@ class TrackManagement:
         talks = self.talks
 
         while talks:
-            print(talks)
             talk = talks[0]
             talks = talks[1:]
             try:
@@ -25,23 +25,9 @@ class TrackManagement:
                 else:
                     track2.schedule_talk(talk)
             except NoEnoughtSpace:
-                talks = self.get_next_talks_iteration([track1,track2], talks, talk)
+                track1 = Track(datetime.now())
+                track2 = Track(datetime.now())
+                random.shuffle(self.talks)
+                talks = self.talks
 
-        self.schedule_network_and_lunch_for_all_tracks([track1, track2])
         return track1, track2
-
-
-    def schedule_network_and_lunch_for_all_tracks(self, tracks):
-        for track in tracks:
-            track.schedule_network_event()
-            track.schedule_lunch_hour()
-
-    def get_next_talks_iteration(self, tracks, talks, current_talk):
-        track1, track2 = tracks
-        added_talks = track1.get_scheduled_talks() + \
-                      track2.get_scheduled_talks() + [current_talk]
-        first_added = added_talks[0]
-        size_talks = len(added_talks)
-        head_talks = added_talks[:size_talks - 1]
-        talks.append(first_added)
-        return head_talks + talks
