df646f898301d47566fbd6505b1af1968a46ab77
Jessica <jlima@thoughtworks.com>
2018-02-01 16:18:12 -0200

    feature: create class to handle with output of conference tracks and talks

---
+++ conference.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Conference:
+
+    def __init__(self, tracks):
+        self.tracks = tracks
+
+    def get_formatted_tracks_for_conference(self):
+        pass
