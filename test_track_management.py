7d7b01c425f8650a4499a4d5d602030be2272408
Jessica <jlima@thoughtworks.com>
2018-02-01 15:00:36 -0200

    refactor: remove unused test

--- test_track_management.py
+++ test_track_management.py
@@ -44,31 +44,6 @@ class TestTrackManagement:
         assert 'Network' in talk_titles[0] and 'Network' in talk_titles[1]
         assert 'Lunch' in talk_titles[0] and 'Lunch' in talk_titles[1]
 
-    def test_safadao(self):
-        talks = [
-            Talk("Writing Fast Tests Against Enterprise Rails", 60),
-            Talk("Overdoing it in Python", 45),
-            Talk("Lua for the Masses", 30),
-            Talk("Ruby Errors from Mismatched Gem Versions", 45),
-            Talk("Common Ruby Errors", 45),
-            Talk("Rails for Python Developers", 5),
-            Talk("Communicating Over Distance", 60),
-            Talk("Accounting-Driven Development", 45),
-            Talk("Woah", 30),
-            Talk("Sit Down and Write", 30),
-            Talk("Pair Programming vs Noise", 45),
-            Talk("Rails Magic", 60),
-            Talk("Ruby on Rails: Why We Should Move On" , 60),
-            Talk("Clojure Ate Scala (on my project)", 45),
-            Talk("Programming in the Boondocks of Seattle", 30),
-            Talk("Ruby vs. Clojure for Back-End Development" ,30),
-            Talk("Ruby on Rails Legacy App Maintenance", 60),
-            Talk("A World Without HackerNews", 30),
-            Talk("User Interface CSS in Rails Apps", 30)
-        ]
-        track_management = TrackManagement(talks)
-        tracks = track_management.generate_tracks_to_talks()
-
     def __get_title_talks(self, tracks):
         talk_titles = []
 
