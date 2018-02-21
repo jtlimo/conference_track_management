723f2a9ec2edaa9fb9013f8ed23c95436db3731b
Jessica Lima <jtl.limao@gmail.com>
2018-02-21 17:15:56 -0300

    create get method and test then

--- tests/test_talks_repository.py
+++ tests/test_talks_repository.py
@@ -8,7 +8,15 @@ class TestTalksRepository:
         talk = Talk("Luna", 60)
         repo = TalksRepository()
 
-        talk_inserted = repo.insert(talk)
+        id_talk = repo.insert(talk)
 
-        assert talk_inserted[0].title == talk.title
-        assert talk_inserted[0].duration == talk.duration
+        assert id_talk == 0
+
+    def test_get_talk_by_id(self):
+        talk = Talk("Biancat", 30)
+        repo = TalksRepository()
+
+        id_talk = repo.insert(talk)
+
+        assert repo.get(id_talk).title == talk.title
+        assert repo.get(id_talk).duration == talk.duration
--- web/talks_repository.py
+++ web/talks_repository.py
@@ -1,7 +1,13 @@
+
+
 class TalksRepository:
     def __init__(self):
         self.talks = []
 
     def insert(self, talk):
         self.talks.append(talk)
-        return self.talks
+        talk_id = len(self.talks) - 1
+        return talk_id
+
+    def get(self, talk_id):
+        return self.talks[talk_id]
