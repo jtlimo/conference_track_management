50ae961dd7e6eafb39185aaa11a8342098df40a6
Jessica <jlima@thoughtworks.com>
2018-03-22 18:09:50 -0300

    [wip] improve generate track method

--- src/track_management.py
+++ src/track_management.py
@@ -15,24 +15,27 @@ class TrackManagement:
         self.tracks_full = []
 
     def generate_tracks_to_talks(self):
-        talks = self.talks
+        #talks = self.talks
+        talks_combinations = self.__get_talks_combinations(self.talks)
         self.tracks.append(Track(datetime.now()))
-        for _ in repeat(talks, factorial(len(talks))):
+        
+        print("combinations",talks_combinations)
+        for talks in talks_combinations:
+            print(talks)
             if not talks:
-                return self.tracks_full + self.tracks
-            talk = talks[0]
-            talks = talks[1:]
-            try:
-                for track in self.tracks:
-                    if not track.is_full():
-                        track.schedule_talk(talk)
-                    else:
-                        self.tracks_full.append(track)
-                        self.tracks.append(Track(datetime.now()))
-                        self.tracks = self.tracks[1:]
-            except NoEnoughtSpace:
-                self.__restart_tracks()
-                talks = self.__get_changed_talks()
+               return self.tracks_full + self.talks    
+            for talk in talks:
+                try:
+                   for track in self.tracks:
+                       if not track.is_full():
+                           track.schedule_talk(talk)
+                       else:
+                           self.tracks_full.append(track)
+                           self.tracks.append(Track(datetime.now()))
+                           self.tracks = self.tracks[1:]
+                except NoEnoughtSpace:
+                    self.__restart_tracks()
+                    #talks = self.__get_changed_talks()
 
         return self.tracks_full + self.tracks
 
@@ -45,3 +48,17 @@ class TrackManagement:
         random.shuffle(self.talks)
         talks = self.talks
         return talks
+        
+    def __get_talks_combinations(self, talks, talks_original_order = [], talks_combinations=[]):
+        if not talks_original_order:
+          talks_original_order = talks.copy()
+        for index, talk in enumerate(talks):
+          i = index + 1
+          if i > len(talks) - 1:
+              if talks == talks_original_order:
+                return talks_combinations
+              self.__get_talks_combinations(talks, talks_original_order)
+          else:
+              talks_combinations.append(talks.copy())
+              talks[index] = talks[index + 1]
+              talks[index + 1] = talk
